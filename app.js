285a70119c6a2b8028c6af1b07fb1a5076d92b45
imranshahzad17 <imranshahzad3454@gmail.com>
2024-03-20 07:25:44 +0500

    Done

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let randomvalue = "1234567890";
-let btn = document.querySelector("button")
-let inp = document.querySelector(".input")
-let password = "";
-
-for(let i = 0; i < 4 ; i++){
-  let randomnum = Math.floor(Math.random() * randomvalue.length);
-  password += randomvalue[randomnum];
-  btn.addEventListener("click",function(){
-    btn.innerHTML ="OTP Generated"
-    inp.value = password;
-  })
+let btn =document.querySelector(".btn")
+btn.addEventListener("click", function() {
+  let input = document.querySelectorAll(".input");
+  let generatedOTP = generateOTP();
+  input.forEach((input, index) => {
+    input.value = generatedOTP[index];
+  });
+});
+function generateOTP() {
+  let otp = "";
+  for (let i = 0; i < 4; i++) {
+    otp += Math.floor(Math.random() * 10);
+  }
+  return otp;
 }
